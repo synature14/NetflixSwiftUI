388fffe8b5ce769c0e0b1994dcc475199f0c97e2
Sutie <shuduck17@gmail.com>
2021-03-19 11:07:16 +0900

    프로젝트 생성

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by SutieDev on 2021/03/19.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
